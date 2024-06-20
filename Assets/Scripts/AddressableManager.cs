b36fe35e7d35048416426963a2a794015abdd27c
DESKTOP-LCD\lcd26 <dan.luongcong@gameloft.com>
2024-06-20 11:50:05 +0700

    + load addressable into scene

--- Assets/Scripts/AddressableManager.cs
+++ Assets/Scripts/AddressableManager.cs
@@ -1,18 +1,73 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
 
 public class AddressableManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private AssetReferenceGameObject refCube;
+
+    [SerializeField]
+    private AssetReferenceTexture2D refLogo;
+
+    [SerializeField]
+    private AssetReferenceAudioClip refClip;
+
+    [SerializeField]
+    private RawImage imageLogo;
+
+    private void Start()
     {
-        
+        refCube.LoadAssetAsync<GameObject>().Completed += OnCubeLoaded;
+        refLogo.LoadAssetAsync<Texture2D>().Completed += OnLogoLoaded;
+        refClip.LoadAssetAsync<AudioClip>().Completed += OnClipLoaded;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        refCube.ReleaseAsset();
+        refLogo.ReleaseAsset();
+        refClip.ReleaseAsset();
+    }
+
+    private static void DebugHandle<TObject>(AsyncOperationHandle<TObject> handle)
+    {
+        Debug.Log($"{handle.DebugName} is loaded {handle.Status}");
+    }
+
+    private void OnClipLoaded(AsyncOperationHandle<AudioClip> handle)
+    {
+        DebugHandle(handle);
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            var go = new GameObject("Background Music");
+            var audio = go.AddComponent<AudioSource>();
+            audio.clip = handle.Result;
+            audio.loop = true;
+            audio.playOnAwake = false;
+            audio.Play();
+        }
+    }
+
+    private void OnLogoLoaded(AsyncOperationHandle<Texture2D> handle)
+    {
+        DebugHandle(handle);
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            imageLogo.texture = handle.Result;
+        }
+    }
+
+    private void OnCubeLoaded(AsyncOperationHandle<GameObject> handle)
+    {
+        DebugHandle(handle);
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            Instantiate(handle.Result);
+        }
     }
 }
---
+++ Assets/Scripts/AssetReferenceAudioClip.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+[Serializable]
+public class AssetReferenceAudioClip : AssetReferenceT<AudioClip>
+{
+    public AssetReferenceAudioClip(string guid) : base(guid)
+    {
+    }
+}
